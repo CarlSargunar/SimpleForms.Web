f9bbf5ad6fc19f13a237bfc3c9e3874038d5e447
Carl Sargunar <carl@sargunar.com>
2023-05-18 00:47:10 +0100

    tests

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Homepage Title', async ({ page }) => {
   await page.goto('https://simple-forms.azurewebsites.net/');
 
   // Test the H1 says Simple Forms
--- tests/contact-form.spec.ts
+++ tests/contact-form.spec.ts
@@ -1,20 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://carlcod.es/');
-
-  // Load the h1 tag to a variable
-  const title = await page.$eval('h1', (el) => el.textContent);
-  // Expect the title to be "Carl's Blog"
-  expect(title).toBe("CarlCod.es");
-});
-
-test('Embedded contact form link', async ({ page }) => {
+test('About Page', async ({ page }) => {
   await page.goto('https://simple-forms.azurewebsites.net/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Contact (Embedded)' }).click();
+  await page.getByRole('link', { name: '(Embedded)' }).click();
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*contact-embedded/);
+  await expect(page).toHaveURL(/.*embedded/);
+
+  // Test the H1 says Simple Forms
+  const title = await page.$eval('h1', (el) => el.textContent);
+  expect(title).toContain("Embedded");
+  
 });
